5b356f1c0aadcf7545b5a0a166dd03d0623149e8
Alejandro <80606321+AlejoProjects@users.noreply.github.com>
2023-06-27 19:27:57 -0500

    Update coder.js

--- coder.js
+++ coder.js
@@ -9,8 +9,126 @@ function impares() {
     rta.innerHTML = num;
 }
 function pokemones() {
-    let rta2 = document.getElementById('respuesta2');
-    rta2.innerHTML = "porfavor abre la consola para ver la respuesta";
+    let answerText = document.getElementById("respuesta2");
+    answerText.textContent = "";
+    let pokemones =
+        ['bulbasaur',
+            'ivysaur',
+            'venusaur',
+            'charmander',
+            'charmeleon',
+            'charizard',
+            'squirtle',
+            'wartortle',
+            'blastoise',
+            'caterpie',
+            'metapod',
+            'butterfree',
+            'weedle',
+            'kakuna',
+            'beedrill',
+            'pidgey',
+            'pidgeotto',
+            'pidgeot',
+            'rattata',
+            'raticate',
+            'spearow',
+            'fearow',
+            'ekans',
+            'arbok',
+            'pikachu',
+            'raichu',
+            'sandshrew',
+            'sandslash',
+            'nidoran-f',
+            'nidorina',
+            'nidoqueen',
+            'nidoran-m',
+            'nidorino',
+            'nidoking',
+            'clefairy',
+            'clefable',
+            'vulpix',
+            'ninetales',
+            'jigglypuff',
+            'wigglytuff',
+            'zubat',
+            'golbat',
+            'oddish',
+            'gloom',
+            'vileplume',
+            'paras',
+            'parasect',
+            'venonat',
+            'venomoth',
+            'diglett',
+            'dugtrio',
+            'meowth',
+            'persian',
+            'psyduck',
+            'golduck',
+            'mankey',
+            'primeape',
+            'growlithe',
+            'arcanine',
+            'poliwag',
+            'poliwhirl',
+            'poliwrath',
+            'abra',
+            'kadabra',
+            'alakazam',
+            'machop',
+            'machoke',
+            'machamp',
+            'bellsprout',
+            'weepinbell',
+            'victreebel',
+            'tentacool',
+            'tentacruel',
+            'geodude',
+            'graveler',
+            'golem',
+            'ponyta',
+            'rapidash',
+            'slowpoke',
+            'slowbro',
+            'magnemite',
+            'magneton',
+            'farfetchd',
+            'doduo',
+            'dodrio',
+            'seel',
+            'dewgong',
+            'grimer',
+            'muk',
+            'shellder',
+            'cloyster',
+            'gastly',
+            'haunter',
+            'gengar',
+            'onix',
+            'drowzee',
+            'hypno',
+            'krabby',
+            'kingler',
+            'voltorb'];
+    const number = parseInt(document.getElementById('numero_pokemones').value);
+    const limit = Math.round(number / 5)* 5;
+    if (isNaN(number)) {
+        alert("porfavor ingrese un numero");
+    }
+    else if(limit > 50 || number < 5){
+        alert("porfavor ingresa un numero mayor a 5 y menor a 50");
+    }
+    else {
+        for (let i = 0; i <= limit; i++) {
+            console.log(pokemones[i]);
+            const textNode = pokemones[i];
+            answerText.append(textNode + ", ");
+        }
+    }
+
+
 
 }
 function arreglos() {
